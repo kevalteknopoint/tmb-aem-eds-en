96593d609bf9b6bf5c13f2c0ded5762c840f1617
Harshal <harshal.malkan@deptagency.com>
2025-09-23 12:07:04 +0530

    tile component done

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,24 +1,94 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
+import Swiper from './swiper-bundle.min.js';
+
+function htmlToElement(htmlString) {
+  const template = document.createElement('template');
+  template.innerHTML = htmlString.trim();
+  return template.content.firstChild;
+}
 
 export default function decorate(block) {
   /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
-  });
-  ul.querySelectorAll('picture > img').forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-    moveInstrumentation(img, optimizedPic.querySelector('img'));
-    img.closest('picture').replaceWith(optimizedPic);
-  });
-  block.textContent = '';
-  block.append(ul);
+  // const ul = document.createElement('ul');
+  // [...block.children].forEach((row) => {
+  //   const li = document.createElement('li');
+  //   moveInstrumentation(row, li);
+  //   while (row.firstElementChild) li.append(row.firstElementChild);
+  //   [...li.children].forEach((div) => {
+  //     if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+  //     else div.className = 'cards-card-body';
+  //   });
+  //   ul.append(li);
+  // });
+  // ul.querySelectorAll('picture > img').forEach((img) => {
+  //   const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+  //   moveInstrumentation(img, optimizedPic.querySelector('img'));
+  //   img.closest('picture').replaceWith(optimizedPic);
+  // });
+  // block.textContent = '';
+  // block.append(ul);
+
+  if (screen.width < 768) {
+    block?.parentElement?.classList.add('swiper');
+    block?.classList.add('swiper-wrapper')
+    block?.parentElement?.insertAdjacentHTML('beforeend', `
+      <div class="swiper-pagination"></div>  
+    `)
+  }
+
+  [...block.children].forEach(row => {
+    const imgCol = row.firstElementChild;
+    const textCol = row.lastElementChild;
+
+    imgCol.classList.add('banner-image');
+    textCol.classList.add('tile-content');
+
+    textCol.firstElementChild.classList.add('content-title');
+    textCol?.querySelector('p:not(.button-container)')?.classList.add('content-description');
+    textCol?.querySelector('p.button-container')?.replaceWith(textCol.lastElementChild.firstElementChild);
+    textCol?.lastElementChild?.classList.add('tile-cta');
+    textCol?.lastElementChild?.classList.remove('button');
+
+    imgCol.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '730' }])));
+
+    const bannerSlide = htmlToElement(`
+      <div class="expandable-tile${screen.width < 768 ? ' swiper-slide' : ''}">${row.innerHTML}</div>  
+    `);
+
+    row.replaceWith(bannerSlide);
+
+    if (screen.width > 768) {
+      bannerSlide?.firstElementChild?.addEventListener('click', function(e) {
+        e.preventDefault();
+  
+        const allSlides = document.querySelectorAll('.expandable-tile');
+  
+        if (bannerSlide?.classList.contains('active')) {
+          bannerSlide?.classList.remove('active')
+        } else {
+          allSlides?.forEach(slide => slide.classList.remove('active'));
+          bannerSlide?.classList.add('active')
+        }
+      })
+    } else {
+      const swiper = new Swiper('.swiper', {
+        slidersPerView: 1,
+        centeredSlides: true,
+        spaceBetween: 24,
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
+    }
+  })
+
+  function documentHandler(e) {
+    if (!e.target.closest('.expandable-tile') && document.querySelector('.expandable-tile.active')) {
+      document.querySelectorAll('.expandable-tile.active')?.forEach(slide => slide.classList.remove('active'));
+    }
+  }
+
+  document.removeEventListener('click', documentHandler)
+  document.addEventListener('click', documentHandler)
 }
