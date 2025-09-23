dc051bafac431663962aa279e9b95c6a2e18941e
gauravibhosale-gb <gauravi.bhosale@deptagency.com>
2025-09-23 20:21:32 +0530

    momentum saver

--- blocks/tmb-teaser/tmb-teaser.js
+++ blocks/tmb-teaser/tmb-teaser.js
@@ -3,13 +3,13 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   /* change to ul, li */
-    const teaserContent = document.querySelector(".default-content-wrapper");
+    const teaserContent = block.querySelector(".default-content-wrapper");
     teaserContent.classList.add("momentum-teaser-wrapper");
 
-    const ulClassname = document.querySelector(".momentum-teaser-wrapper ul");
+    const ulClassname = block.querySelector(".momentum-teaser-wrapper ul");
     ulClassname.classList.add("momentum-listing-wrapper");
 
-    const secondulClassname = document.querySelector(".momentum-teaser-wrapper").nextElementSibling;
+    const secondulClassname = block.querySelector(".momentum-teaser-wrapper").nextElementSibling;
     secondulClassname.classList.add("listing-wrapper");
 
 }
