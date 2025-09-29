9df01b72f1e41a7a247f4fd013a38b1af1fbbd99
Harshal <harshal.malkan@deptagency.com>
2025-09-29 17:19:48 +0530

    js update for default comp

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -140,5 +140,25 @@ export default function decorate(block) {
 
     // document.removeEventListener('click', documentHandler);
     // document.addEventListener('click', documentHandler);
+  } else {
+    // online banking help js starts
+    const cardsUl = block.querySelector('.online-banking .cards-wrapper .cards ul');
+    cardsUl.classList.add('banking-cards-ul');
+    block.querySelectorAll('.banking-cards-ul > li').forEach((li, idx) => {
+      li.classList.add(`banking-li-${idx + 1}`);
+    });
+    block.querySelectorAll('.online-banking .cards-wrapper .cards ul li:nth-child(2) .cards-card-body ul').forEach((ul, idx) => {
+      ul.classList.add(`card-bottom-${idx + 1}`);
+    });
+    block.querySelectorAll('.online-banking .cards-wrapper .cards ul li:nth-child(2) .cards-card-body li').forEach((li, idx) => {
+      li.classList.add(`banking-desc-${idx + 1}`);
+    });
+    block.querySelectorAll('.online-banking .cards-wrapper .cards ul li:nth-child(3) .cards-card-body ul').forEach((ul, idx) => {
+      ul.classList.add(`card-bottom-${idx + 1}`);
+    });
+    block.querySelectorAll('.online-banking .cards-wrapper .cards ul li:nth-child(3) .cards-card-body li').forEach((li, idx) => {
+      li.classList.add(`banking-desc-${idx + 1}`);
+    });
+    // online banking help js end
   }
 }
