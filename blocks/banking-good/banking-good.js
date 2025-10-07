2b9fd0770faa1ecacc220d7aff9530ab0d3f29e0
Puneetprakash sharma <puneetprakash.sharma@deptagency.com>
2025-10-07 15:02:05 +0530

    js and css added

--- blocks/banking-good/banking-good.js
+++ blocks/banking-good/banking-good.js
@@ -43,4 +43,8 @@ export default async function decorate(block) {
 
   wrapper.appendChild(ul);
   block.append(wrapper);
+  const bottomUl = document.querySelectorAll('.banking-good-wrapper:not(.card-1) .banking-good-item:nth-child(3) >div >ul >li');
+  bottomUl.forEach((wrapper, index) => {
+    wrapper.classList.add(`bottom-li-${index + 1}`);
+  });
 }
