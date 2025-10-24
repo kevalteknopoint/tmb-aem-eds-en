c60ad93dbfce98aff5db98fcad582d97b747fa31
Harshal <harshal.malkan@deptagency.com>
2025-10-24 09:11:35 +0530

    faq update

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,18 +1,24 @@
-export default function decorate(block) {
-  const cols = [...block.firstElementChild.children];
-  block.classList.add(`columns-${cols.length}-cols`);
+import decorateFaqBanner from "./faq-banner.js";
 
-  // setup image columns
-  [...block.children].forEach((row) => {
-    [...row.children].forEach((col) => {
-      const pic = col.querySelector('picture');
-      if (pic) {
-        const picWrapper = pic.closest('div');
-        if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
+export default function decorate(block) {
+  if (block.closest('.faq-landing-banner')) {
+    decorateFaqBanner(block);
+  } else {
+    const cols = [...block.firstElementChild.children];
+    block.classList.add(`columns-${cols.length}-cols`);
+
+    // setup image columns
+    [...block.children].forEach((row) => {
+      [...row.children].forEach((col) => {
+        const pic = col.querySelector('picture');
+        if (pic) {
+          const picWrapper = pic.closest('div');
+          if (picWrapper && picWrapper.children.length === 1) {
+            // picture is only content in column
+            picWrapper.classList.add('columns-img-col');
+          }
         }
-      }
+      });
     });
-  });
+  }
 }
