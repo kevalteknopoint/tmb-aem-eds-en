73c0292ca3ba5de38a1d77987525b24f4edcedf5
gauravibhosale-gb <gauravi.bhosale@deptagency.com>
2025-10-27 18:46:00 +0530

    faq category

--- blocks/faq-category/faq-category.js
+++ blocks/faq-category/faq-category.js
@@ -1,3 +1,16 @@
-export default function decorate() {
+export default function decorate(block) {
+  // Create a new div element
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('questions-wrapper'); // Add your desired class name
 
+  // Select all <ul> elements inside the block
+  const uls = block.querySelectorAll('ul');
+
+  // Move all <ul> elements into the wrapper
+  uls.forEach((ul) => {
+    wrapper.appendChild(ul);
+  });
+
+  // Insert the wrapper into the DOM
+  block.appendChild(wrapper);
 }
