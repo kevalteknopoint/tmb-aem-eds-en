553123875ab25e368c2b610a36b80eaee66ed4f0
gauravibhosale-gb <gauravi.bhosale@deptagency.com>
2025-10-27 19:17:21 +0530

    faq category

--- blocks/faq-category/faq-category.js
+++ blocks/faq-category/faq-category.js
@@ -1,18 +1,18 @@
-export default function decorate(block) {
-  // Select the <p> element inside the block
-  const p = block.querySelector('p');
-  if (!p) return;
+// export default function decorate(block) {
+//   // Select the <p> element inside the block
+//   const p = block.querySelector('p');
+//   if (!p) return;
 
-  // Create a new div and give it a class
-  const wrapper = document.createElement('div');
-  wrapper.classList.add('questions-container');
+//   // Create a new div and give it a class
+//   const wrapper = document.createElement('div');
+//   wrapper.classList.add('questions-container');
 
-  // Insert the new div right after the <p> tag
-  p.insertAdjacentElement('afterend', wrapper);
+//   // Insert the new div right after the <p> tag
+//   p.insertAdjacentElement('afterend', wrapper);
 
-  // Move all <ul> elements into the new div
-  const uls = block.querySelectorAll('ul');
-  uls.forEach((ul) => {
-    wrapper.appendChild(ul);
-  });
-}
+//   // Move all <ul> elements into the new div
+//   const uls = block.querySelectorAll('ul');
+//   uls.forEach((ul) => {
+//     wrapper.appendChild(ul);
+//   });
+// }
