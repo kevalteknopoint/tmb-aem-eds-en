ea08e594c1798ec4aa8cde62cd89eb2ba580a086
gauravibhosale-gb <gauravi.bhosale@deptagency.com>
2025-10-28 12:12:13 +0530

    faq swiper implementation

--- blocks/faq-category/faq-category.js
+++ blocks/faq-category/faq-category.js
@@ -1,18 +1,67 @@
-// export default function decorate(block) {
-//   // Select the <p> element inside the block
-//   const p = block.querySelector('p');
-//   if (!p) return;
+import Swiperblock from "../../libs/swiper/swiper-bundle.min.js";
 
-//   // Create a new div and give it a class
-//   const wrapper = document.createElement('div');
-//   wrapper.classList.add('questions-container');
+function createSwiper(block) {
+  if (!block.classList.contains("swiper")) {
+    block.classList.add("swiper");
+    const rows = Array.from(block.children.children);
+    const swiperWrapper = document.createElement("div");
+    swiperWrapper.classList.add("swiper-wrapper");
+    rows.forEach((row) => {
+      const towrapdeskandmob = document.createElement("div");
+      towrapdeskandmob.classList.add("mob-desk-wrapper");
+      row.lastElementChild?.setAttribute('data-aos', 'fade-up');
+      row.lastElementChild?.setAttribute('data-aos-duration', '2000');
 
-//   // Insert the new div right after the <p> tag
-//   p.insertAdjacentElement('afterend', wrapper);
+      const desktopDiv = row.children[0];
+      const mobDiv = row.children[1];
+      const richtextdiv = row.children[2];
+      row.classList.add("swiper-slide");
+      row.classList.add(`swiperinnerdiv`); // ${i + 1}
+      swiperWrapper.append(row);
+      desktopDiv.classList.add("desktop-banner");
+      mobDiv.classList.add("mob-pbanner");
+      richtextdiv.classList.add("richtext-class");
 
-//   // Move all <ul> elements into the new div
-//   const uls = block.querySelectorAll('ul');
-//   uls.forEach((ul) => {
-//     wrapper.appendChild(ul);
-//   });
-// }
+      towrapdeskandmob.appendChild(desktopDiv);
+      towrapdeskandmob.appendChild(mobDiv);
+      row.append(towrapdeskandmob);
+      // appendclasses.CLASS_PREFIXES = ['mainswrapper'];
+      // appendclasses.addIndexed(row)
+      // Array.from(row.children).forEach((child, i) => {
+      //   if (child.tagName === "DIV" && child.innerHTML.trim() === "") {
+      //     child.remove();
+      //   }
+      // });
+    });
+    block.append(swiperWrapper);
+    const swiperpagination = document.createElement("div");
+    swiperpagination.classList.add("swiper-pagination");
+    const swiperpaginationouter = document.createElement("div");
+    swiperpaginationouter.classList.add("outer-pegination-div"); // for play btn
+    const playbtn = document.createElement("button");
+    playbtn.classList.add("play-btn");
+    const iconp = document.createElement("img");
+    iconp.src = "../../icons/Vector.svg";
+    iconp.alt = "play Icon";
+    playbtn.appendChild(iconp);
+    swiperpaginationouter.append(swiperpagination);
+    swiperpaginationouter.append(playbtn);
+    block.append(swiperpaginationouter);
+  }
+}
+export default function decorate(block) {
+}
+let block = document.querySelector(".section.faq-container.faq-category-container")
+createSwiper(block);
+    const swiper = new Swiperblock(block, {
+      slidesPerView: 1,
+      spaceBetween: 2,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay: {
+        delay: 1000,
+        disableOnInteraction: false,
+      },
+    });
