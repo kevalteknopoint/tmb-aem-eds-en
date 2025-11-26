273f97f8db14c20797392ee79b4e00887ce27dc9
Harshal <harshal.malkan@deptagency.com>
2025-11-26 11:44:28 +0530

    js update

--- blocks/cards/banking-goods.js
+++ blocks/cards/banking-goods.js
@@ -5,7 +5,7 @@ export default function decorateBankingGoods(block) {
   const allListItems = block.querySelectorAll('li');
 
   allListItems?.forEach((item, index) => {
-    if (window.innerWidth <= 767 && index === 0) {
+    if (window.innerWidth <= 1023 && index === 0) {
       const contentWrapper = div({ class: 'banking-goods-mb' });
       contentWrapper.innerHTML = item.innerHTML;
       block.insertAdjacentElement('beforebegin', contentWrapper);
@@ -15,7 +15,7 @@ export default function decorateBankingGoods(block) {
     const cardBtn = item?.querySelector('.button-container a');
     injectIcon('chevron-right-links', cardBtn);
 
-    if (window.innerWidth <= 767 || index === 0) return;
+    if (window.innerWidth <= 1023 || index === 0) return;
 
     const cardBody = item?.querySelector('.cards-card-body');
     const cardHeading = cardBody?.querySelector('h1, h2, h3, h4, h5, h6');
