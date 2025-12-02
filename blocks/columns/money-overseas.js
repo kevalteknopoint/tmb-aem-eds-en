60478e871bfbd228bba036b3e94bbff6b09bec41
gauravibhosale-gb <gauravi.bhosale@deptagency.com>
2025-12-02 13:04:37 +0530

    money overseas component

---
+++ blocks/columns/money-overseas.js
@@ -0,0 +1,4 @@
+export default function decorateMoneyOverseas(){
+const moneyContainer = document.querySelector(".money-overseas .default-content-wrapper");
+moneyContainer?.classList?.add("overseas-columns");
+}
