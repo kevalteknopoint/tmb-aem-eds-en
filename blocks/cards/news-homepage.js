911a7489089097277d56fe75c3a74e4aa97586fe
Puneetprakash sharma <puneetprakash.sharma@deptagency.com>
2025-12-04 13:36:34 +0530

    news and helpful component wip

--- blocks/cards/news-homepage.js
+++ blocks/cards/news-homepage.js
@@ -2,7 +2,9 @@
 // import { injectIcon } from "../../scripts/aem.js";
 // import { button, div } from "../../scripts/dom-helpers.js";
 
-// export default function cardCarousel(block) {
+// export default function cardCarouselHomepage(block) {
+//     console.log("-dkjak");
+
 //   const slides = Array.from(block.children);
 //   slides.forEach((el) => {
 //     el.classList.add("swiper-slide");
@@ -33,11 +35,8 @@
 
 //   block.replaceWith(container);
 
-//   // eslint-disable-next-line
-//     // ---------------------------
-//   // ✅ Initialize Swiper with BREAKPOINTS
-//   // ---------------------------
-//   const swiper = new Swiper(container, {
+
+// const swiper = new Swiper(container, {
 //     slidesPerView: 1,
 //     // spaceBetween: 16,
 //     navigation: {
@@ -46,7 +45,7 @@
 //     },
 //     breakpoints: {
 //       400: {  
-//         slidesPerView: 1.5,
+//         slidesPerView: 1,
 //         spaceBetween: 8
 //       },
 //       640: {    
@@ -54,24 +53,25 @@
 //         spaceBetween: 8
 //       },
 //       834: {
-//         slidesPerView: 3,
+//         slidesPerView: 2.5,
 //       },
 //       1024: { 
-//         slidesPerView: 3.5,
+//         slidesPerView: 3,
 //       },
 //       1400: {
-//         slidesPerView: 4.5,
+//         slidesPerView: 3.5,
 //       }
 //     }
 //   });
+// };
 
-// }
+
 
 import Swiper from "../../libs/swiper/swiper-bundle.min.js";
 import { injectIcon } from "../../scripts/aem.js";
 import { button, div } from "../../scripts/dom-helpers.js";
 
-export default function cardCarousel(block) {
+export default function cardCarouselHomepage(block) {
   const slides = Array.from(block.children);
   slides.forEach((el) => {
     el.classList.add("swiper-slide");
@@ -103,32 +103,64 @@ export default function cardCarousel(block) {
   block.replaceWith(container);
 
   // eslint-disable-next-line
-  const swiper = new Swiper(container, {
+//   const swiper = new Swiper(container, {
+//     slidesPerView: 1,
+//     spaceBetween: 16,
+//     navigation: {
+//       nextEl: nextBtn,
+//       prevEl: prevBtn,
+//     },
+//     breakpoints: {
+//       400: {
+//         slidesPerView: 1.5,
+//       },
+//       640: {
+//         slidesPerView: 2,
+//         spaceBetween: 24
+//       },
+//       834: {
+//         slidesPerView: 3,
+//       },
+//       1024: {
+//         slidesPerView: 3.5,
+//         spaceBetween: 32
+//       },
+//       1400: {
+//         slidesPerView: 4,
+//         spaceBetween: 32
+//       }
+//     }
+//   });
+
+const swiper = new Swiper(container, {
     slidesPerView: 1,
-    spaceBetween: 16,
+    // spaceBetween: 16,
     navigation: {
       nextEl: nextBtn,
       prevEl: prevBtn,
     },
     breakpoints: {
-      400: {
-        slidesPerView: 1.5,
+      400: {  
+        slidesPerView: 1,
+        spaceBetween: 16
       },
-      640: {
+      640: {    
         slidesPerView: 2,
-        spaceBetween: 24
+        spaceBetween: 16
       },
       834: {
-        slidesPerView: 3,
+        slidesPerView: 2.5,
+        spaceBetween: 24
       },
-      1024: {
-        slidesPerView: 3.5,
-        spaceBetween: 32
+      1024: { 
+        slidesPerView: 3,
+        spaceBetween: 24
       },
       1400: {
-        slidesPerView: 4,
+        slidesPerView: 3.5,
         spaceBetween: 32
       }
     }
   });
+
 }
