cee79d9b8e11e11051ead4647a9866a1a99706cb
Harshal <harshal.malkan@deptagency.com>
2025-12-10 18:52:34 +0530

    js fix

--- blocks/momentum-saver/momentum-saver.js
+++ blocks/momentum-saver/momentum-saver.js
@@ -2,7 +2,7 @@ export default function decorateMomentumSaver() {
   const allMomentumSections = document.querySelectorAll('.momentum-saver-section');
 
   allMomentumSections?.forEach((momentumSection) => {
-    const momentumWrapper = momentumSection?.querySelector("& .columns");
+    const momentumWrapper = momentumSection?.querySelector(".columns");
     momentumWrapper?.classList?.add("momentum-columns");
 
     const momentumWrapperImg = momentumSection?.querySelector(".momentum-image-saver .columns");
@@ -11,25 +11,32 @@ export default function decorateMomentumSaver() {
     const momentumWrapperImgSwap = momentumSection?.querySelector(".image-swapping .columns");
     momentumWrapperImgSwap?.classList?.add("momentum-columns");
 
-    const momentumWrapperdiv = momentumSection?.querySelector("&.left-percentage-section .momentum-columns > div > div");
-    momentumWrapperdiv?.classList?.add("momentum-content");
+    if (momentumSection?.classList?.contains("left-percentage-section")) {
+      const momentumWrapperdiv = momentumSection.querySelector(".momentum-columns > div > div");
+      momentumWrapperdiv?.classList?.add("momentum-content");
+    }
 
-    const momentumWrapperseconddiv = momentumSection?.querySelector("& .momentum-columns > div > .momentum-content + div");
+    const momentumWrapperseconddiv = momentumSection?.querySelector(".momentum-columns > div > .momentum-content + div");
     momentumWrapperseconddiv?.classList?.add("momentum-second-content");
 
-    const momentumWrapperSecondDivSaver = momentumSection?.querySelector("&.momentum-image-saver > .columns-wrapper > .momentum-columns > div > div:nth-child(2)");
-    momentumWrapperSecondDivSaver?.classList?.add("momentum-second-content");
+    if (momentumSection?.classList?.contains("momentum-image-saver")) {
+      const momentumWrapperSecondDivSaver = momentumSection.querySelector(".columns-wrapper > .momentum-columns > div > div:nth-child(2)");
+      momentumWrapperSecondDivSaver?.classList?.add("momentum-second-content");
 
-    const momentumWrapperSecondDivSaverSwap = momentumSection?.querySelector("&.momentum-image-saver.image-swapping > .columns-wrapper > .momentum-columns > div > div:nth-child(2)");
-    momentumWrapperSecondDivSaverSwap?.classList?.add("momentum-second-content");
+      if (momentumSection?.classList?.contains("image-swapping")) {
+        const momentumWrapperSecondDivSaverSwap = momentumSection.querySelector(".columns-wrapper > .momentum-columns > div > div:nth-child(2)");
+        momentumWrapperSecondDivSaverSwap?.classList?.add("momentum-second-content");
+      }
+    }
 
-    const variantText = momentumSection?.querySelector("&.background-color-teal");
-    if (variantText) {
-      const varText = momentumSection?.querySelector("&.background-color-teal > .columns-wrapper > .momentum-columns");
+    const isTeal = momentumSection?.classList?.contains("background-color-teal");
+
+    if (isTeal) {
+      const varText = momentumSection.querySelector(".columns-wrapper > .momentum-columns");
       varText?.classList?.add("background-color-white");
     }
 
-    const interestRateWrap = momentumSection?.querySelector('& p:has(.interest-rate)');
+    const interestRateWrap = momentumSection?.querySelector('p:has(.interest-rate)');
     if (!interestRateWrap) return;
 
     const interestRate = interestRateWrap?.querySelector('.interest-rate');
