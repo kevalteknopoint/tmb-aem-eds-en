f37e2ea24c3a156d8d9b900421bd7387f0449232
gauravibhosale-gb <gauravi.bhosale@deptagency.com>
2025-12-11 17:46:35 +0530

    bugfix

--- blocks/columns/money-overseas.js
+++ blocks/columns/money-overseas.js
@@ -1,3 +1,6 @@
+import { injectIcon } from "../../scripts/aem.js";
+import { div } from "../../scripts/dom-helpers";
+
 export default function decorateMoneyOverseas() {
   // Add .overseas-wrapper to the content wrapper
   document
@@ -19,5 +22,77 @@ export default function decorateMoneyOverseas() {
   document
     .querySelectorAll(".money-overseas .overseas-columns  ul li ul")
     .forEach((el) => el.classList.add("overseas-columns-wrapper-ul"));
+    // const overseasWrapperdiv =document.querySelector(".overseas-variant .overseas-columns .overseas-columns-wrapper > div:nth-of-type(2)");
+    // overseasWrapperdiv?.classList?.add("overseas-content-div");
+    //  injectIcon('special-list-indicator');
+//     const overseasWrapperdiv = document.querySelector(
+//   ".overseas-variant .overseas-columns .overseas-columns-wrapper > div:nth-of-type(2)"
+// );
 
+// if (overseasWrapperdiv) {
+//   overseasWrapperdiv.classList.add("overseas-content-div");
+
+//   // find the <p> inside
+//   const p = overseasWrapperdiv.querySelector("p");
+//   if (p) {
+//     // create wrapper div
+//     const wrapper = document.createElement("div");
+//     wrapper.classList.add("content-with-icon");
+
+//     // move <p> inside wrapper
+//     wrapper.appendChild(p);
+
+//     // create icon using your function
+//     const icon = injectIcon('special-list-indicator');
+
+//     // append the icon
+//     wrapper.appendChild(icon);
+
+//     // insert wrapper into the original div
+//     overseasWrapperdiv.appendChild(wrapper);
+//   }
+// }
+// const targetDiv = document.querySelector(
+//   ".overseas-variant .overseas-columns .overseas-columns-wrapper > div:nth-of-type(2)"
+// );
+
+// if (targetDiv) {
+//   const p = targetDiv.querySelector("p");
+
+//   if (p) {
+//     // REMOVE the p from its original location
+//     p.remove();
+
+//     // Create wrapper
+//     const wrapper = document.createElement("div");
+//     wrapper.classList.add("content-with-icon");
+
+//     // Create icon span FIRST
+//     const iconSpan = document.createElement("span");
+//     iconSpan.classList.add("icon", "icon-checkmark");
+
+//     // Append span then p
+//     wrapper.appendChild(iconSpan);
+//     wrapper.appendChild(p);
+
+//     // Insert wrapper back into the div
+//     targetDiv.appendChild(wrapper);
+//   }
+// }
+
+const targetColumns = document.querySelectorAll(".overseas-columns-wrapper");
+
+targetColumns?.forEach(col => {
+  const textCol = col?.querySelector('div:last-child');
+  const paragraphs = textCol.querySelectorAll("p");
+
+  paragraphs.forEach(pTag => {
+    if (!pTag?.querySelector("span.icon")) return;
+
+    const innerSpan = pTag.querySelector("span.icon")?.cloneNode(true);
+    pTag.querySelector('span.icon')?.remove();
+    const newDiv = div({ class: 'content-with-icon' }, innerSpan, p(...pTag?.children));
+    pTag.replaceWith(newDiv);
+  });
+})
 }
