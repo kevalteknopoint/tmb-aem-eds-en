5d323a7392faa6d53e802f601f359f9aa4a7fec1
akanksha-1103 <akansha.kasar@deptagency.com>
2025-12-15 18:44:17 +0530

    css changes

--- blocks/cards/looing-for-anotherway.js
+++ blocks/cards/looing-for-anotherway.js
@@ -1,3 +1,12 @@
 export default function decorateLookingAnotherway(block) {
   console.log(block);
+  document.querySelectorAll(".cards li").forEach((li) => {
+    const contentP = li.querySelector("p:not(.button-container)");
+    if (contentP) {
+      contentP.classList.add("card-text");
+    }
+  });
+  document.querySelectorAll(".cards li p.button-container").forEach((p) => {
+    p.classList.add("card-button");
+  });
 }
