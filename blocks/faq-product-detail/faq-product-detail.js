37651effae36fbfa9a5212afe114419fc2de0ce4
Harshal <harshal.malkan@deptagency.com>
2026-01-02 23:35:54 +0530

    faq block updated in product

--- blocks/faq-product-detail/faq-product-detail.js
+++ blocks/faq-product-detail/faq-product-detail.js
@@ -29,70 +29,54 @@ function createAccordionItem(question, answerHtml) {
 
 /* ---------- RENDER FAQ ---------- */
 function renderFAQ(block, faq) {
-  const section = block.closest(".faq-productdetail-container");
-  block.textContent = "";
-
-  /* LEFT SIDE */
-  const leftCol = div(
-    { class: "faq-left" },
-    h2("FREQUENTLY ASKED QUESTIONS")
-  );
-
   /* Normalize data → always array */
   const faqItems = Array.isArray(faq)
     ? faq
     : [faq];
 
-  /* RIGHT SIDE (MATCHES YOUR HTML) */
-  const accordion = div(
-    {
-      class: "accordion single-expansion block",
-      "data-block-name": "accordion",
-      "data-block-status": "loaded",
-    },
-    ...faqItems.map((item) =>
-      createAccordionItem(
-        item.question,
-        item.shortDescription?.html
-      )
+  block.append(...faqItems.map((item) =>
+    createAccordionItem(
+      item.question,
+      item.shortDescription?.html
     )
-  );
-
-  const rightCol = div(
-    { class: "faq-right" },
-    div({ class: "accordion-wrapper" }, accordion)
-  );
-
-  block.append(
-    div({ class: "faq-layout" }, leftCol, rightCol)
-  );
-
-  if (section) {
-    section.style.display = "";
-    section.dataset.sectionStatus = "loaded";
-  }
+  ));
 }
 
 /* ---------- DECORATE ---------- */
 export default async function decorate(block) {
-  const secwrapper = document.querySelector(".faq-productdetail-wrapper");
+  const secwrapper = document.querySelector(".faq-product-detail-wrapper");
+
   if (!secwrapper) return;
 
   const placeholders = await fetchPlaceholders();
   const graphqlUrl = `${placeholders.graphqlurl}faqShortContent;path=`;
 
   try {
-    const fragUrl = block.querySelector("a")?.getAttribute("href");
-    if (!fragUrl) return;
+    const newBlock = div({ class: 'accordion-wrapper' });
+    const accordion = div(
+      {
+        class: "accordion single-expansion block",
+        "data-block-name": "accordion",
+        "data-block-status": "loaded",
+      },
+    );
 
-    const res = await fetch(`${graphqlUrl}${fragUrl}`);
-    const data = await res.json();
-    const faq = data?.data?.faqByPath?.item;
+    for (const item of [...block.children]) {
+      const fragUrl = item.querySelector("a")?.getAttribute("href");
+      if (!fragUrl) return;
+  
+      const res = await fetch(`${graphqlUrl}${fragUrl}`);
+      const data = await res.json();
+      const faq = data?.data?.faqByPath?.item;
+  
+      if (!faq) return;
+  
+      renderFAQ(accordion, faq);
+    }
 
-    if (!faq) return;
-
-    console.log("FAQ DATA:", faq);
-    renderFAQ(block, faq);
+    block.innerHTML = '';
+    newBlock.appendChild(accordion);
+    block.appendChild(newBlock);
   } catch (err) {
     console.error("Error loading FAQ:", err);
   }
