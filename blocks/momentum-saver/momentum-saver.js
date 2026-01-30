790a81ea32998f702e991b3c135c8a7b4ee359ae
Harshal <harshal.malkan@deptagency.com>
2026-01-30 14:50:23 +0530

    styling updates

--- blocks/momentum-saver/momentum-saver.js
+++ blocks/momentum-saver/momentum-saver.js
@@ -3,7 +3,7 @@ import { div } from "../../scripts/dom-helpers.js";
 (function decorateMomentumSaver() {
   if (window.location.origin.includes('author')) return;
 
-  const momentumSaverSections = document.querySelectorAll('.momentum-saver-section');
+  const momentumSaverSections = document.querySelectorAll('.momentum-saver-section, .momentum-image-saver, .image-swapping, .momentum-app-badges, .momentum-direct-variant');
 
   momentumSaverSections.forEach((section) => {
     const newWrapper = div({ class: 'momentum-section-wrapper' });
