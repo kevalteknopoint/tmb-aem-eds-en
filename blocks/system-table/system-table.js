0f163e2c21068715d2d210b84c0d4c582c6bc451
Puneetprakash sharma <puneetprakash.sharma@deptagency.com>
2026-02-12 19:50:37 +0530

    system page

--- blocks/system-table/system-table.js
+++ blocks/system-table/system-table.js
@@ -1,3 +1,23 @@
-export default function decorate(block) {
+(function decorateLegalTcGuide() {
+    if (window.location.origin.includes('author')) return;
     console.log("system");
-}
+  const block = document.querySelector('.system-table');
+
+  if (!block) return;
+  if (block) {
+    const statusItems = document.querySelectorAll('.system-table li li');
+
+    statusItems.forEach(item => {
+        const text = item.textContent.trim();
+        if (text === 'Service Available') {
+                item.classList.add('status-available');
+        } 
+        else if (text === 'Service Unavailable') {
+            item.classList.add('status-unavailable');
+        } 
+        else if (text === 'Service Interruptions') {
+            item.classList.add('status-interruptions');
+        }
+    });
+  }
+}());
