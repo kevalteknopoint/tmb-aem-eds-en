41271e8797e144418c30df53ea73ad99afdb3c9c
Puneetprakash sharma <puneetprakash.sharma@deptagency.com>
2026-02-13 12:00:45 +0530

    system alert modified

--- blocks/alerts/alerts.js
+++ blocks/alerts/alerts.js
@@ -2,17 +2,34 @@ export default function decorate(block) {
   if (window.location.href.includes("author")) return;
 
   [...block.children].forEach((row) => {
-    // if icon present
-    if (row.querySelector(".icon, svg")) {
+    const iconElement = row.querySelector(".icon, svg");
+
+    if (iconElement) {
       if (
-        row.querySelector(".icon, svg").className.includes("icon-close-icon")
+        row.querySelector(".icon-close-icon") ||
+        iconElement.classList.contains("icon-close-icon")
       ) {
         row.classList.add("icon-close");
       } else {
         row.classList.add("icon");
       }
     } else {
-      row.classList.add("alert-text");
+      if (row.textContent.trim() !== "") {
+        row.classList.add("alert-text");
+      } else {
+        row.remove();
+      }
     }
+  });
+
+  const closeIcons = block.querySelectorAll(".icon-close");
+  closeIcons.forEach((icon) => {
+    icon.addEventListener("click", (event) => {
+      const alertDiv = event.currentTarget.closest(".alerts");
+
+      if (alertDiv) {
+        alertDiv.style.display = "none";
+      }
+    });
   });
 }
