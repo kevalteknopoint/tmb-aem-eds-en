0918c0f176340aab5884c1a781f4b5798e1801d8
Puneetprakash sharma <puneetprakash.sharma@deptagency.com>
2026-02-16 13:26:27 +0530

    date modification

--- blocks/mortage-insurance/mortage-insurance.js
+++ blocks/mortage-insurance/mortage-insurance.js
@@ -1,1 +1,11 @@
-console.log('mortage-insurance block loaded');
+(function decorateMortgageInsurance(block) {
+  if (window.location.href.includes("author")) return;
+  debugger;
+  console.log('decorating mortage-insurance block');
+  console.log(...block.allChildren);
+  const children = [...block.allChildren];
+
+  children.forEach((child) => {
+    console.log(child);
+  });
+}(document.querySelector('.mortgage-insurance')));
