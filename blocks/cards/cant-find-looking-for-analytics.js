e9c698cc8721f60fc9abdc905ab8e82045e4a477
Harshal <harshal.malkan@deptagency.com>
2026-02-24 14:37:20 +0530

    cta fix

--- blocks/cards/cant-find-looking-for-analytics.js
+++ blocks/cards/cant-find-looking-for-analytics.js
@@ -1,4 +1,4 @@
-import { ctaInteraction, minifyText,getComponentIndex,getPageRegion,getPersona } from "../../scripts/analytics/exports.js";
+import { ctaInteraction, minifyText, getComponentIndex, getPageRegion, getPersona } from "../../scripts/analytics/exports.js";
 
 document.addEventListener('click', (e) => {
   const { target } = e;
@@ -12,12 +12,11 @@ document.addEventListener('click', (e) => {
       const titleContainer = targetContainer?.querySelector('.default-content-wrapper');
       const title = titleContainer?.querySelector('h1, h2, h3, h4, h5, h6');
       const ctaTitleEle = linkEle?.closest('.cards-card-body')?.querySelector('h1, h2, h3, h4, h5, h6');
-      const pageRegion = getPageRegion(e.target.closest('.cards-wrapper')?.querySelector("ul li a"));
-      const componentIndex = getComponentIndex(e.target.closest('.cards-wrapper .button-container')?.querySelector("a"));
-      const nextPageURL = (e.target.closest('.cards-wrapper .button-container')?.getAttribute("href"));
+      const pageRegion = getPageRegion(linkEle);
+      const componentIndex = getComponentIndex(linkEle);
+      const nextPageURL = (linkEle)?.getAttribute("href");
 
-      ctaInteraction(pageRegion, ctaText, minifyText(ctaTitleEle?.textContent), 'can’t find what you are looking for?', minifyText(title?.textContent), '', componentIndex,getPersona(),nextPageURL,'cta-click','external','in-page-nav','in-content','','','','can’t find what you are looking for?','');
+      ctaInteraction(pageRegion, ctaText, minifyText(ctaTitleEle?.textContent), 'can’t find what you are looking for?', minifyText(title?.textContent), '', componentIndex, getPersona(), nextPageURL, 'cta-click', 'external', 'in-page-nav', 'in-content', '', '', '', 'can’t find what you are looking for?', '');
     }
   }
 });
-
