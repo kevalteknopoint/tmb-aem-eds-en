439aa6011763deb2f4b90d5921649fbfd2972038
gauravibhosale-gb <gauravi.bhosale@deptagency.com>
2026-02-25 12:36:17 +0530

    analytics

--- blocks/faq-category/faq-category-analytics.js
+++ blocks/faq-category/faq-category-analytics.js
@@ -1,4 +1,4 @@
-import { ctaInteraction, faqInteraction, minifyText, getComponentIndex, getPageRegion, getPersona } from "../../scripts/analytics/exports.js";
+import { ctaInteraction, faqInteraction, minifyText, getComponentIndex, getPageRegion, getPersona} from "../../scripts/analytics/exports.js";
 
 document.addEventListener('click', (e) => {
   const { target } = e;
@@ -10,13 +10,28 @@ document.addEventListener('click', (e) => {
       const targetContainer = target.closest('.faq-frequently-question');
       const titleContainer = targetContainer?.previousElementSibling;
       const title = titleContainer?.querySelector('h1, h2, h3, h4, h5, h6');
-      const ctaSourceEle = linkEle?.closest('.faq-frequently-question-title')?.parentElement?.querySelector('h1, h2, h3, h4, h5, h6');
+      const ctaSourceEle = linkEle?.closest('.faq-items-list')?.parentElement?.querySelector('h1, h2, h3, h4, h5, h6');
       const pageRegion = getPageRegion(target.closest('.faq-link'));
       const componentIndex = getComponentIndex(target.closest('.faq-link'));
-      faqInteraction(pageRegion, faqTitle, minifyText(ctaSourceEle?.textContent), 'faq-frequently', 'faq', componentIndex, getPersona(), 'faq-toggle', 'faq-expand', 'FAQ_CARD_LIMITS', 'open', 'faq swift code', '', '', '', '', '', '', '', '', 'faq', '');
-    }
+      faqInteraction(pageRegion, faqTitle, minifyText(ctaSourceEle?.textContent), minifyText(title?.textContent), 'faq-frequently', '',componentIndex,getPersona(),'faq-toggle','faq-expand','FAQ_CARD_LIMITS','','open','','faq swift code','','','','','','faq','');
+    } else if (target.closest('.faq-frequently-question-list')) {
+      const targetContainer = target.closest('.faq-frequently-question-list');
+      const titleContainer = targetContainer?.previousElementSibling;
+      const title = titleContainer?.querySelector('h1, h2, h3, h4, h5, h6');
+      const ctaSourceEle = linkEle?.closest('.faq-items-list')?.parentElement?.querySelector('h1, h2, h3, h4, h5, h6');
+      const pageRegion = getPageRegion(target.closest('.faq-link'));
+      const componentIndex = getComponentIndex(target.closest('.faq-link'));
 
-    if (target.closest('.faq-frequently-question-list')) {
+      faqInteraction(pageRegion, faqTitle, minifyText(ctaSourceEle?.textContent), minifyText(title?.textContent), 'faq-frequently', '',componentIndex,getPersona(),'faq-toggle','faq-expand','FAQ_CARD_LIMITS','','open','','faq swift code','','','','','','faq','');
+    }
+  }
+
+
+  if (target.closest('a') && target.closest('.button-container')) {
+    const linkEle = target.closest('a');
+    const ctaText = minifyText(linkEle?.textContent);
+
+       if (target.closest('.faq-frequently-question-list')) {
       const targetContainer = target.closest('.faq-frequently-question-list');
       const secondaryLink = e.target.closest('.faq-frequently-question-list .button-container .button');
       // const titleContainer = targetContainer?.previousElementSibling;
@@ -27,24 +42,13 @@ document.addEventListener('click', (e) => {
       const nextPageURL = e.target.closest(".faq-frequently-question-list .button-container .button")?.getAttribute("href");
 
       ctaInteraction(pageRegion, minifyText(secondaryLink?.textContent), minifyText(ctaTitle?.textContent),minifyText(ctaSourceEle?.textContent), 'faq category', 'faq', componentIndex, getPersona(),nextPageURL, 'cta-click', 'internal', 'in-page-nav', 'in-content', '', '', '', 'faq', '');
-    }
-  }
- 
-  if (target.closest('a') && target.closest('.button-container')) {
-    const linkEle = target.closest('a');
-    const ctaText = minifyText(linkEle?.textContent);
-
-    if (target.closest('.faq-frequently-question-list')) {
-      const targetContainer = target.closest('.faq-frequently-question-list');
-      const titleContainer = targetContainer?.previousElementSibling;
+    } else if (target.closest('.faq-cant-find-looking-for')) {
+      const targetContainer = target.closest('.faq-cant-find-looking-for');
+      const titleContainer = targetContainer?.querySelector('.default-content-wrapper');
       const title = titleContainer?.querySelector('h1, h2, h3, h4, h5, h6');
-      const ctaSourceEle = linkEle?.closest('.button-container')?.parentElement?.querySelector('h1, h2, h3, h4, h5, h6');
-      const pageRegion = getPageRegion(target.closest('.faq-link'));
-      const componentIndex = getComponentIndex(target.closest('.faq-link'));
-      const nextPageURL = target.closest('.button-container')?.getAttribute("href");
+      const ctaTitleEle = linkEle?.closest('.cards-card-body')?.querySelector('h1, h2, h3, h4, h5, h6');
 
-
-      ctaInteraction(pageRegion, ctaText, minifyText(ctaSourceEle?.textContent), minifyText(title?.textContent), 'fragment-container', 'cards', componentIndex, getPersona(), nextPageURL, 'cta-click', 'external', 'in-page-nav', 'in-content', '', '', '');
+      ctaInteraction('', ctaText, minifyText(ctaTitleEle?.textContent), '', minifyText(title?.textContent), '', '');
     }
   }
 });
