9af40fed3b6bd7fdc1132ae9b5f769aacb279c96
Harshal <harshal.malkan@deptagency.com>
2026-03-02 13:08:15 +0530

    tabbed component completed

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,4 +1,54 @@
+import { button, div, p } from "../../scripts/dom-helpers.js";
+
 export default async function decorate(block) {
   if (window.location.origin.includes('author')) return;
-  console.log(block);
+
+  const allPanels = document.querySelectorAll('.tabs-panel');
+  const tabList = div({ class: 'tabs-list', role: 'tablist' });
+  const tabPanels = div({ class: 'tabs-panels' });
+  const currentPanelIds = [];
+
+  [...block.children].forEach((tabItem, index) => {
+    const tabTitle = tabItem.firstElementChild?.textContent;
+    const tabId = tabItem.lastElementChild?.textContent;
+    const tabButton = button({ class: 'tabs-tab', type: 'button', role: 'tab', id: `${tabId}--tab`, 'aria-controls': `${tabId}--panel`, 'aria-selected': index === 0 ? 'true' : 'false' }, p(tabTitle));
+
+    currentPanelIds.push(`${tabId}--panel`);
+
+    tabList.appendChild(tabButton);
+
+    tabButton.addEventListener("click", () => {
+      const tabPanel = document.getElementById(`${tabId}--panel`);
+
+      tabPanels.querySelectorAll("[role=tabpanel]").forEach((panel) => {
+        panel.setAttribute("aria-hidden", "true");
+      });
+
+      tabList.querySelectorAll("[role=tab]").forEach((btn) => {
+        btn.setAttribute("aria-selected", "false");
+      });
+
+      tabPanel.setAttribute("aria-hidden", "false");
+      tabButton.setAttribute("aria-selected", "true");
+    });
+  });
+
+  allPanels.forEach((panel) => {
+    if (!currentPanelIds?.includes(`${panel.id}--panel`)) return;
+
+    panel.setAttribute('id', `${panel.id}--panel`);
+    panel.setAttribute('aria-hidden', 'false');
+    panel.setAttribute('aria-labelledby', `${panel.id}--tab`);
+    panel.setAttribute('role', 'tabpanel');
+
+    tabPanels.appendChild(panel);
+  });
+
+  block.innerHTML = '';
+
+  const tabListWrapper = div({ class: 'tab-list-wrapper' }, tabList);
+  block.appendChild(tabListWrapper);
+  block.appendChild(tabPanels);
+
+  tabList.children?.[0]?.dispatchEvent(new Event('click'));
 }
