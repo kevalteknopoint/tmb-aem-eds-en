337e8bae072d9da79d7d722add34b877e9c9f31e
gauravibhosale-gb <gauravi.bhosale@deptagency.com>
2026-03-09 15:53:03 +0530

    analytics  cta changes

--- blocks/cards/cant-find-looking-for-analytics.js
+++ blocks/cards/cant-find-looking-for-analytics.js
@@ -7,7 +7,7 @@ document.addEventListener('click', (e) => {
     const linkEle = target.closest('a');
     const ctaText = minifyText(linkEle?.textContent);
 
-    if (target.closest('.faq-cant-find-looking-for')) {
+    if (target.closest('.faq-cant-find-looking-for.rates-saver')) {
       const targetContainer = target.closest('.faq-cant-find-looking-for');
       const titleContainer = targetContainer?.querySelector('.default-content-wrapper');
       const title = titleContainer?.querySelector('h1, h2, h3, h4, h5, h6');
@@ -15,10 +15,11 @@ document.addEventListener('click', (e) => {
       const pageRegion = getPageRegion(linkEle);
       const componentIndex = getComponentIndex(linkEle);
       const nextPageURL = (linkEle)?.getAttribute("href");
-
+      console.log('rtyu');
+      
       ctaInteraction(pageRegion, ctaText, minifyText(ctaTitleEle?.textContent), 'cant find what you are looking for?', minifyText(title?.textContent), '', componentIndex, getPersona(), nextPageURL, 'cta-click', 'external', 'in-page-nav', 'in-content', '', '', '', 'cant find what you are looking for?', '');
     }
-    if (target.closest('.faq-cant-find-looking-for.faq-cant-find-looking-for-variant')) {
+    if (e.target.closest('.faq-cant-find-looking-for.faq-cant-find-looking-for-variant:not(.rates-saver)')) {
       const targetContainer = target.closest('.faq-cant-find-looking-for.faq-cant-find-looking-for-variant');
       const titleContainer = targetContainer?.querySelector('.default-content-wrapper');
       const title = titleContainer?.querySelector('h1, h2, h3, h4, h5, h6');
@@ -26,7 +27,7 @@ document.addEventListener('click', (e) => {
       const pageRegion = getPageRegion(linkEle);
       const componentIndex = getComponentIndex(linkEle);
       const nextPageURL = (linkEle)?.getAttribute("href");
-
+      console.log('dfghj')
       ctaInteraction(pageRegion, ctaText, minifyText(ctaTitleEle?.textContent), 'WAYS TO GET IN TOUCH', minifyText(title?.textContent), '', componentIndex, getPersona(), nextPageURL, 'cta-click', 'external', 'in-page-nav', 'in-content', '', '', '', 'WAYS TO GET IN TOUCH', '');
     }
   }
