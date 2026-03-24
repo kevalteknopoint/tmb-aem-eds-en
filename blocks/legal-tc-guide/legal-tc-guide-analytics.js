40bfe46ad7f24e81be0240c50cb97419a1ff9129
gauravibhosale-gb <gauravi.bhosale@deptagency.com>
2026-03-24 16:03:27 +0530

    pdf download

--- blocks/legal-tc-guide/legal-tc-guide-analytics.js
+++ blocks/legal-tc-guide/legal-tc-guide-analytics.js
@@ -2,39 +2,71 @@ import { ctaInteraction, minifyText, getPersona, getPageRegion, getComponentInde
 
 document.addEventListener('click', (e) => {
   const link = e.target.closest('.legal-tc-guide a');
-  if (!link) return;
+  if (link) {
+    const block = link.closest('.legal-tc-guide');
 
-  const block = link.closest('.legal-tc-guide');
+    const pageRegion = getPageRegion(link);
+    const componentIndex = getComponentIndex(block);
+    const ctaTitle = block.querySelector('h1, h2, h3, h4, h5, h6');
 
-  const pageRegion = getPageRegion(link);
-  const componentIndex = getComponentIndex(block);
-  const ctaTitle = block.querySelector('h1, h2, h3, h4, h5, h6');
+    const ctaText = minifyText(link.textContent);
+    const nextPageURL = link.href;
+    ctaInteraction(
+      pageRegion,
+      ctaText,
+      minifyText(ctaTitle?.textContent),
+      '',
+      'legal tc guide',
+      'legal tc guide',
+      componentIndex,
+      getPersona(),
+      nextPageURL,
+      'cta-click',
+      'internal',
+      'quick-link',
+      'internal',
+      '',
+      '',
+      '',
+      'legaltc',
+      ''
+    );
+  };
 
-  const ctaText = minifyText(link.textContent);
-  const nextPageURL = link.href;
 
-  console.log("clicked link:", link);
-  console.log("text:", ctaText);
-  console.log("url:", nextPageURL);
 
-  ctaInteraction(
-    pageRegion,
-    ctaText,
-    minifyText(ctaTitle?.textContent),
-    '',
-    'legal tc guide',
-    'legal tc guide',
-    componentIndex,
-    getPersona(),
-    nextPageURL,
-    'cta-click',
-    'internal',
-    'quick-link',
-    'internal',
-    '',
-    '',
-    '',
-    'legaltc',
-    ''
-  );
+  const faqLink = e.target.closest('.faq-legal a');
+
+  if (faqLink) {
+    const container = faqLink.closest('.faq-legal');
+    const ctaText = minifyText(faqLink.innerText || '');
+    const accordionItem = faqLink.closest('.accordion-item');
+    const sectionTitle = accordionItem?.querySelector('summary p');
+    const ctaTitle = minifyText(sectionTitle?.innerText || '');
+    const pageRegion = getPageRegion(faqLink);
+    const allLinks = Array.from(container.querySelectorAll('a'));
+    const componentIndex = allLinks.indexOf(faqLink) + 1;
+    const nextPageURL = faqLink.getAttribute('href') || '';
+
+    ctaInteraction(
+      pageRegion,
+      ctaText,
+      ctaTitle,
+      '',
+      'faq legal',
+      'faq legal',
+      componentIndex,
+      getPersona(),
+      nextPageURL,
+      'cta-click',
+      'internal',
+      'accordion-link',
+      'internal',
+      '',
+      '',
+      '',
+      'faqlegal',
+      ''
+    );
+  }
 });
