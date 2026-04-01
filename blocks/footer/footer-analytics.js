52d71e7fd352f59f90168b89e9ab2e67aee4e8bb
gauravibhosale-gb <gauravi.bhosale@deptagency.com>
2026-04-01 11:28:00 +0530

    issues

--- blocks/footer/footer-analytics.js
+++ blocks/footer/footer-analytics.js
@@ -3,43 +3,37 @@ import { ctaInteraction, menuInteraction, minifyText, socialmediaClick, getCompo
 document.addEventListener('click', (e) => {
   if (e.target.closest('.footer-col')) {
     const anchor = e.target.closest('a');
-     const icon = anchor.querySelector('.icon');
+    const icon = anchor.querySelector('.icon');
     if (!anchor) return;
     if (!icon) {
-      
-    
-    const componentIndex = getComponentIndex(anchor);
-    const nextPageURL = anchor?.getAttribute('href') || '';
-
-    const closestLi = anchor?.closest('ul')?.closest('li');
-
-    let text = "";
-    if (closestLi) {
-      closestLi.childNodes?.forEach((node) => {
-        if (node.nodeType === Node.TEXT_NODE) {
-          text += minifyText(node.textContent);
-        }
-      });
+      const componentIndex = getComponentIndex(anchor);
+      const nextPageURL = anchor?.getAttribute('href') || '';
+      const closestLi = anchor?.closest('ul')?.closest('li');
+      let text = "";
+      if (closestLi) {
+        closestLi.childNodes?.forEach((node) => {
+          if (node.nodeType === Node.TEXT_NODE) {
+            text += minifyText(node.textContent);
+          }
+        });
+      }
+      if (!text && closestLi?.querySelector(':scope > p')) {
+        text = minifyText(closestLi?.querySelector(':scope > p')?.textContent);
+      }
+      menuInteraction('bottom', minifyText(text), minifyText(anchor.textContent), '', 'global footer', 'footer', componentIndex, getPersona(), nextPageURL, 'menu-click', 'internal', '', '', '', 'footer', '');
     }
-    if (!text && closestLi?.querySelector(':scope > p')) {
-      text = minifyText(closestLi?.querySelector(':scope > p')?.textContent);
-    }
-    menuInteraction('bottom', minifyText(text), minifyText(anchor.textContent), '', 'global footer', 'footer', componentIndex, getPersona(), nextPageURL, 'menu-click', 'internal', '', '', '', 'footer', '');}
   }
-
   if (e.target.closest('.footer-links')) {
     const anchor = e.target.closest('a');
     if (!anchor) return;
-
     const icon = anchor.querySelector('.icon');
     let iconClassString;
     let iconName;
     if (icon) {
       iconClassString = icon.classList.toString();
       iconName = iconClassString?.replaceAll('icon-', '')?.replaceAll('icon', '')?.replaceAll(' ', '');
-       const componentIndex = getComponentIndex(anchor);
-    socialmediaClick('bottom', minifyText(iconName), 'global footer', 'footer', componentIndex, getPersona(), 'socialmedia-click', '', '', '', 'global footer', '');
-  
+      const componentIndex = getComponentIndex(anchor);
+      socialmediaClick('bottom', minifyText(iconName), 'global footer', 'footer', componentIndex, getPersona(), 'socialmedia-click', '', '', '', 'global footer', '');
     }
   }
   if (e.target.closest('.footer-bottom')) {
