f04324f8ee988e1ce26c00fa4e95cd8be2982d89
gauravibhosale-gb <gauravi.bhosale@deptagency.com>
2026-04-01 11:05:48 +0530

    issues

--- blocks/footer/footer-analytics.js
+++ blocks/footer/footer-analytics.js
@@ -4,8 +4,6 @@ document.addEventListener('click', (e) => {
   if (e.target.closest('.footer-col')) {
     const anchor = e.target.closest('a');
     if (!anchor) return;
-
-    const pageRegion = getPageRegion(anchor);
     const componentIndex = getComponentIndex(anchor);
     const nextPageURL = anchor?.getAttribute('href') || '';
 
@@ -22,7 +20,7 @@ document.addEventListener('click', (e) => {
     if (!text && closestLi?.querySelector(':scope > p')) {
       text = minifyText(closestLi?.querySelector(':scope > p')?.textContent);
     }
-    menuInteraction(pageRegion, minifyText(text), minifyText(anchor.textContent), '', 'global footer', 'footer', componentIndex, getPersona(), nextPageURL, 'menu-click', 'internal', '', '', '', 'footer', '');
+    menuInteraction('bottom', minifyText(text), minifyText(anchor.textContent), '', 'global footer', 'footer', componentIndex, getPersona(), nextPageURL, 'menu-click', 'internal', '', '', '', 'footer', '');
   }
 
   if (e.target.closest('.footer-meta-item')) {
@@ -36,9 +34,18 @@ document.addEventListener('click', (e) => {
       iconClassString = icon.classList.toString();
       iconName = iconClassString?.replaceAll('icon-', '')?.replaceAll('icon', '')?.replaceAll(' ', '');
     }
-    const pageRegion = getPageRegion(anchor);
     const componentIndex = getComponentIndex(anchor);
-    socialmediaClick(pageRegion, minifyText(iconName), 'global footer', 'footer', componentIndex, getPersona(), 'socialmedia-click', '', '', '', 'global footer', '');
+    socialmediaClick('bottom', minifyText(iconName), 'global footer', 'footer', componentIndex, getPersona(), 'socialmedia-click', '', '', '', 'global footer', '');
+  }
+
+    if (e.target.closest('.footer-bottom')) {
+    const anchor = e.target.closest('a');
+    if (!anchor) return;
+
+    const anchorLi = anchor?.closest('li');
+    const componentIndex = getComponentIndex(anchor);
+    const nextPageURL = anchor?.getAttribute('href') || '';
+    ctaInteraction('bottom', minifyText(anchorLi?.textContent), minifyText(anchor?.getAttribute('title')), 'menu-click', 'global footer', 'footer', componentIndex, getPersona(), nextPageURL, 'cta-click', 'internal', 'link', 'footer', '', '', '', 'global footer', '');
   }
 
   if (e.target.closest('.footer-contact')) {
@@ -46,9 +53,8 @@ document.addEventListener('click', (e) => {
     if (!anchor) return;
 
     const anchorLi = anchor?.closest('li');
-    const pageRegion = getPageRegion(anchor);
     const componentIndex = getComponentIndex(anchor);
     const nextPageURL = anchor?.getAttribute('href') || '';
-    ctaInteraction(pageRegion, minifyText(anchorLi?.textContent), minifyText(anchor?.getAttribute('title')), 'socialmedia-click', 'global footer', 'footer', componentIndex, getPersona(), nextPageURL, 'cta-click', 'internal', 'link', 'footer', '', '', '', 'global footer', '');
+    ctaInteraction('bottom', minifyText(anchorLi?.textContent), minifyText(anchor?.getAttribute('title')), 'socialmedia-click', 'global footer', 'footer', componentIndex, getPersona(), nextPageURL, 'cta-click', 'internal', 'link', 'footer', '', '', '', 'global footer', '');
   }
 });
